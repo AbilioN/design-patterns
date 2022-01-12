c71beb468168791023fb7b747af6fb4d8907ff7c
AbilioN <netobalby@gmail.com>
2022-01-12 14:06:56 -0300

    remove vendor

---
+++ src/CalculadoraDeImpostos.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Alura\DesignPatterns;
+
+class CalculadoraDeImpostos
+{
+
+
+    public function calcula(Orcamento $orcamento , string $nomeImposto): float
+    {
+        switch ($nomeImposto)
+        {
+            case "ICMS":
+                return $orcamento->valor * 0.1;
+            case "ISS":
+                return $orcamento->valor * 0.06;
+        }
+        return $orcamento->valor * 0.1;
+    }
+}
